7cb2c9be48a2ce82343c68103bf23ecfbb76c012
AndreyJustino <andreyjustino2020@gmail.com>
2024-12-01 20:35:29 -0300

    feat: codigo inicial do projeto

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        char answer = 'S';
+
+        while (answer == 'S'){
+            System.out.print("Gostaria de ver os livros disponiveis?");
+            answer = sc.next().toUpperCase().charAt(0);
+
+            /*Local que vai fica a renderização do livro*/
+
+            System.out.print("Qual o ID do livro que você deseja?: ");
+            int id = sc.nextInt();
+
+            System.out.print("Confirme o nome do livro em questão: ");
+            String name = sc.nextLine();
+
+        }
+
+        System.out.println("Fim da aplicação.");
+
+        sc.close();
     }
 }
