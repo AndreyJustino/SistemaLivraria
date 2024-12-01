e4822a3df90579f7cdfe1789f93fe785366a9d05
AndreyJustino <andreyjustino2020@gmail.com>
2024-12-01 20:53:08 -0300

    feat: add estrutura base de classe library

---
+++ src/entities/Library.java
@@ -0,0 +1,43 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    private List<Book> books = new ArrayList<>();
+    private List<Author> authors = new ArrayList<>();
+    private List<Lend> lends = new ArrayList<>();
+
+    public Library() {
+    }
+
+    public Library(List<Book> books, List<Author> authors, List<Lend> lends) {
+        this.books = books;
+        this.authors = authors;
+        this.lends = lends;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    public List<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<Author> authors) {
+        this.authors = authors;
+    }
+
+    public List<Lend> getLends() {
+        return lends;
+    }
+
+    public void setLends(List<Lend> lends) {
+        this.lends = lends;
+    }
+}
